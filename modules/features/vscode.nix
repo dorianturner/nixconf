6b2ab15e899fb213c2f2cf0fee5722aa24c3ba58
Dorian Turner <dorianemmanuelturner@gmail.com>
2026-03-29 22:10:51 +0100

    feat: vscode extensions for nixos dev

--- modules/features/vscode.nix
+++ modules/features/vscode.nix
@@ -3,7 +3,16 @@
 {
   flake.nixosModules.vscode = { pkgs, ... }: {
 
-    programs.vscode.enable = true;
+    programs.vscode = {
+      enable = true;
 
+      extensions = with pkgs.vscode-extensions; [
+        jdinhlife.gruvbox
+        vscodevim.vim
+        yzhang.markdown-all-in-one
+        bbenoist.nix
+      ];
+    
+    };
   };
 }
