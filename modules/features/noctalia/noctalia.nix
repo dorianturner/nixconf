281c9580c3878b12732ee24a472f7d3ffa2840f6
Dorian Turner <dorianemmanuelturner@gmail.com>
2026-04-16 21:30:42 +0100

    feat: some minor configuration of noctalia shell

--- modules/features/noctalia/noctalia.nix
+++ modules/features/noctalia/noctalia.nix
@@ -6,5 +6,6 @@
     ];
 
     hjem.users.dorian.files.".config/noctalia/config.json".source = ./noctalia.json;
+    hjem.users.dorian.files."Pictures/Wallpapers/nix.png".source = ./Wallpapers/nix.png;
   };
 }
