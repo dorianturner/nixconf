e98f70317dd6d2fc4eb7607d5c6ed12c0321f40a
Dorian Turner <dorianemmanuelturner@gmail.com>
2026-04-24 20:13:59 +0100

    feat: printer discovery

--- modules/hosts/asus-tuf/configuration.nix
+++ modules/hosts/asus-tuf/configuration.nix
@@ -40,6 +40,12 @@
     # Enable CUPS to print documents.
     services.printing.enable = true;
 
+    services.avahi = {
+      enable = true;
+      nssmdns4 = true;
+      openFirewall = true;
+    };
+
     # Upower 
     services.upower.enable = true;
     
